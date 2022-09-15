71b08847b86538b835df23d893513c2ebcaad94f
Andrew Exton <andrew@affinitymws.com>
2022-09-14 23:04:40 -0700

    better acceleration/deceleration and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, transform};
 use bevy_prototype_lyon::prelude::*;
 
 // Defines the amount of time that should elapse between each physics step.
@@ -11,13 +11,13 @@ const PLAYER_SIZE: Vec3 = Vec3::new(100.0, 100.0, 0.0);
 
 #[derive(Component)]
 struct Player {
-    dX: f32,
-    dY: f32
+    delta_x: f32,
+    delta_y: f32
 }
 
 impl Player {
     fn new() -> Player {
-        Player { dX: 0.0, dY: 0.0 }
+        Player { delta_x: 0.0, delta_y: 0.0 }
     }
 }
 
@@ -36,7 +36,7 @@ fn setup(mut commands: Commands) {
 
     let player_shape = shapes::RegularPolygon {
         sides: 3,
-        feature: shapes::RegularPolygonFeature::Radius(100.0),
+        feature: shapes::RegularPolygonFeature::Radius(20.0),
         ..shapes::RegularPolygon::default()
     };
 
@@ -46,7 +46,7 @@ fn setup(mut commands: Commands) {
             &player_shape,
             DrawMode::Outlined {
                 fill_mode: FillMode::color(Color::CYAN),
-                outline_mode: StrokeMode::new(Color::WHITE, 10.0),
+                outline_mode: StrokeMode::new(Color::WHITE, 2.0),
             },
             Transform::default(),
         ));
@@ -58,40 +58,40 @@ fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
     mut player_query: Query<(&mut Player, &mut Transform)>
 ) {
-    const ACCELERATION: f32 = 1.0;
-    const DECLERATION: f32 = 0.75;
+    const ACCELERATION: f32 = 0.5;
+    const DECLERATION: f32 = 0.95;
     const MAX_VELOCITY: f32 = 16.0;
 
     let (mut player, mut trans) = player_query.single_mut();
 
     if keyboard_input.pressed(KeyCode::Left) {
-        player.dX -= ACCELERATION;
+        player.delta_x -= ACCELERATION;
     }
 
     if keyboard_input.pressed(KeyCode::Right) {
-        player.dX += ACCELERATION;
+        player.delta_x += ACCELERATION;
     }
 
     if keyboard_input.pressed(KeyCode::Up) {
-        player.dY += ACCELERATION;
+        player.delta_y += ACCELERATION;
     }
 
     if keyboard_input.pressed(KeyCode::Down) {
-        player.dY -= ACCELERATION;
+        player.delta_y -= ACCELERATION;
     }
 
-    player.dX = player.dX.clamp(-MAX_VELOCITY, MAX_VELOCITY);
-    player.dY = player.dY.clamp(-MAX_VELOCITY, MAX_VELOCITY);
+    player.delta_x = player.delta_x.clamp(-MAX_VELOCITY, MAX_VELOCITY);
+    player.delta_y = player.delta_y.clamp(-MAX_VELOCITY, MAX_VELOCITY);
 
-    trans.translation.x += player.dX;
-    trans.translation.y += player.dY;
+    trans.translation.x += player.delta_x;
+    trans.translation.y += player.delta_y;
 
     trans.translation.x = trans.translation.x.clamp(-320.0, 320.0);
     trans.translation.y = trans.translation.y.clamp(-320.0, 320.0);
 
     // Decelerate
-    player.dX *= DECLERATION;
-    player.dY *= DECLERATION;
+    player.delta_x *= DECLERATION;
+    player.delta_y *= DECLERATION;
 }
 
 
