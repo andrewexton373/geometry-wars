dca6f5036a410baaf91a9693a669bbe8f7a020ac
Andrew Exton <andrew@affinitymws.com>
2022-09-14 22:06:43 -0700

    triangle can now move with rudimentary acceleration and deceleration.

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,15 @@ const PLAYER_SIZE: Vec3 = Vec3::new(100.0, 100.0, 0.0);
 
 #[derive(Component)]
 struct Player {
-    position: Position,
-    rotation: Rotation
+    dX: f32,
+    dY: f32
 }
 
-#[derive(Component)]
-struct Position {
-    x: f32,
-    y: f32
+impl Player {
+    fn new() -> Player {
+        Player { dX: 0.0, dY: 0.0 }
+    }
 }
-
-#[derive(Component)]
-struct Rotation {
-    angle: f32
-}
-
 
 fn main() {
     App::new()
@@ -33,7 +27,7 @@ fn main() {
         .add_plugin(ShapePlugin)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
-        .add_system(movement)
+        .add_system(player_movement)
         .run();
 }
 
@@ -47,10 +41,7 @@ fn setup(mut commands: Commands) {
     };
 
     commands.spawn()
-        .insert(Player {
-            position: Position { x: 0.0, y: 0.0 },
-            rotation: Rotation { angle: 0.0 }
-        })
+        .insert(Player::new())
         .insert_bundle(GeometryBuilder::build_as(
             &player_shape,
             DrawMode::Outlined {
@@ -60,12 +51,47 @@ fn setup(mut commands: Commands) {
             Transform::default(),
         ));
         // .insert(Collider);
-
-        
     
 }
 
-fn movement(mut commands: Commands) {
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<(&mut Player, &mut Transform)>
+) {
+    const ACCELERATION: f32 = 1.0;
+    const DECLERATION: f32 = 0.75;
+    const MAX_VELOCITY: f32 = 16.0;
 
+    let (mut player, mut trans) = player_query.single_mut();
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        player.dX -= ACCELERATION;
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        player.dX += ACCELERATION;
+    }
+
+    if keyboard_input.pressed(KeyCode::Up) {
+        player.dY += ACCELERATION;
+    }
+
+    if keyboard_input.pressed(KeyCode::Down) {
+        player.dY -= ACCELERATION;
+    }
+
+    player.dX = player.dX.clamp(-MAX_VELOCITY, MAX_VELOCITY);
+    player.dY = player.dY.clamp(-MAX_VELOCITY, MAX_VELOCITY);
+
+    trans.translation.x += player.dX;
+    trans.translation.y += player.dY;
+
+    trans.translation.x = trans.translation.x.clamp(-320.0, 320.0);
+    trans.translation.y = trans.translation.y.clamp(-320.0, 320.0);
+
+    // Decelerate
+    player.dX *= DECLERATION;
+    player.dY *= DECLERATION;
 }
+
 
