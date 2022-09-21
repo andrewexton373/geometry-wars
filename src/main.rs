a6d689624a48982541f058d3b476853d4e2ca3eb
andrewexton373 <aexton@calpoly.edu>
2022-09-21 00:53:22 -0700

    refactored player spawn, movement, and firing to plugin.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use bevy_inspector_egui::{InspectorPlugin, Inspectable, RegisterInspectable};
 use bevy_inspector_egui::WorldInspectorPlugin;
 use rand::Rng;
 
+mod player;
+use player::{ PlayerPlugin, Player };
+
 // Defines the amount of time that should elapse between each physics step.
 const TIME_STEP: f32 = 1.0 / 60.0;
 
@@ -15,33 +18,14 @@ const PLAYER_COLOR: Color = Color::rgb(1.0, 0.0, 0.0);
 
 const PLAYER_SIZE: Vec3 = Vec3::new(100.0, 100.0, 0.0);
 
-const PI: f32 = 3.14159;
-const TWO_PI: f32 = 2.0 * PI;
-
-
-#[derive(Component, Inspectable)]
-struct Player {
-    delta_x: f32,
-    delta_y: f32,
-    delta_rotation: f32,
-    hitbox: HitboxCircle
-}
-
-impl Player {
-    fn new() -> Player {
-        Player { 
-            delta_x: 0.0,
-            delta_y: 0.0,
-            delta_rotation: 0.0,
-            hitbox: HitboxCircle { radius: 5.0 }
-        }
-    }
-}
+pub const PI: f32 = 3.14159;
+pub const TWO_PI: f32 = 2.0 * PI;
 
 #[derive(Component, Inspectable)]
-struct HitboxCircle {
-    radius: f32
+pub struct HitboxCircle {
+    pub radius: f32
 }
+
 
 #[derive(Component)]
 struct Projectile {
@@ -58,13 +42,12 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(PlayerPlugin)
         .register_inspectable::<Player>()
         .add_plugin(ShapePlugin)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(setup)
-        .add_system(player_movement)
         .add_system(camera_follows_player)
-        .add_system(player_fire_weapon)
         .add_system(projectile_movement)
         .add_system(projectile_collision_check)
         .add_startup_system(spawn_astroids)
@@ -74,28 +57,6 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     let camera = commands.spawn_bundle(Camera2dBundle::default()).id();
-
-    let player_shape = shapes::RegularPolygon {
-        sides: 3,
-        feature: shapes::RegularPolygonFeature::Radius(20.0),
-        ..shapes::RegularPolygon::default()
-    };
-
-    let mut player = commands.spawn()
-        .insert(Player::new())
-        .insert_bundle(GeometryBuilder::build_as(
-            &player_shape,
-            DrawMode::Outlined {
-                fill_mode: FillMode::color(Color::CYAN),
-                outline_mode: StrokeMode::new(Color::WHITE, 2.0),
-            },
-            Transform {
-                scale: Vec3::new(0.5, 1.0, 1.0),
-                ..Default::default()
-            }
-            // Transform::default(),
-        ));
-
         // .insert(Collider);
     
     let line = shapes::Line(
@@ -131,114 +92,6 @@ fn camera_follows_player(
         camera_trans.translation -= player_to_camera;
 }
 
-fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    // need to get window dimensions
-    wnds: Res<Windows>,
-    // query to get camera transform
-    q_camera: Query<(&Camera, &GlobalTransform)>,
-    mut player_query: Query<(&mut Player, &mut Transform), Without<Crosshair>>,
-    mut crosshair_query: Query<(&mut Crosshair, &mut Path, &mut Transform)>
-) {
-
-    // get the camera info and transform
-    // assuming there is exactly one main camera entity, so query::single() is OK
-    let (camera, camera_transform) = q_camera.single();
-
-    let (crosshair, mut path, mut crosshair_transform) = crosshair_query.single_mut();
-
-    // get the window that the camera is displaying to (or the primary window)
-    let wnd = if let RenderTarget::Window(id) = camera.target {
-        wnds.get(id).unwrap()
-    } else {
-        wnds.get_primary().unwrap()
-    };
-
-    const ACCELERATION: f32 = 0.2;
-    const DECLERATION: f32 = 0.95;
-    const SPIN_ACCELERATION: f32 = 0.4;
-    const SPIN_DECELERATION: f32 = 0.1;
-    const MAX_VELOCITY: f32 = 6.0;
-
-    let (mut player, mut player_trans) = player_query.single_mut();
-
-    if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-        player.delta_x -= ACCELERATION;
-    }
-
-    if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-        player.delta_x += ACCELERATION;
-    }
-
-    if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-        player.delta_y += ACCELERATION;
-    }
-
-    if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-        player.delta_y -= ACCELERATION;
-    }
-
-     // check if the cursor is inside the window and get its position
-     if let Some(screen_pos) = wnd.cursor_position() {
-        // get the size of the window
-        let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
-
-        // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
-        let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
-
-        // matrix for undoing the projection and camera transform
-        let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
-
-        // use it to convert ndc to world-space coordinates
-        let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
-
-        // reduce it to a 2D value
-        let world_pos: Vec2 = world_pos.truncate();
-
-        let player_to_mouse = Vec2::new(player_trans.translation.x, player_trans.translation.y) - world_pos;
-        let ship_angle_difference = Vec2::angle_between(player_to_mouse, (player_trans.rotation * Vec3::Y).truncate());
-
-        //Rotate towards position mouse is on
-        if ship_angle_difference > 0.0 {
-            player.delta_rotation += SPIN_ACCELERATION * (TWO_PI - ship_angle_difference.abs());
-        } else
-
-        if ship_angle_difference < 0.0 {
-            player.delta_rotation -= SPIN_ACCELERATION * (TWO_PI - ship_angle_difference.abs());
-        }
-
-        // Draw Crosshair
-        {
-            let line = shapes::Line (
-                player_trans.translation.truncate(),
-                world_pos,
-            );
-    
-            *path = ShapePath::build_as(&line);
-        }
-        
-        // eprintln!("World coords: {}/{}", world_pos.x, world_pos.y);
-    }
-
-    player.delta_x = player.delta_x.clamp(-MAX_VELOCITY, MAX_VELOCITY);
-    player.delta_y = player.delta_y.clamp(-MAX_VELOCITY, MAX_VELOCITY);
-    player.delta_rotation = player.delta_rotation.clamp(-MAX_VELOCITY, MAX_VELOCITY);
-
-    player_trans.translation.x += player.delta_x;
-    player_trans.translation.y += player.delta_y;
-
-    // player_trans.translation.x = player_trans.translation.x.clamp(-320.0, 320.0);
-    // player_trans.translation.y = player_trans.translation.y.clamp(-320.0, 320.0);
-
-    player_trans.rotate_z(player.delta_rotation.to_radians());
-
-    // Decelerate
-    player.delta_x *= DECLERATION;
-    player.delta_y *= DECLERATION;
-    player.delta_rotation *= SPIN_DECELERATION;
-
-}
-
 fn projectile_movement(
     mut commands: Commands,
     mut projectile_query: Query<(Entity, &mut Projectile, &mut Transform)>,
@@ -254,42 +107,6 @@ fn projectile_movement(
         if projectile.timer.finished() {
             commands.entity(ent).despawn_recursive();
         }
-    }
-}
-
-fn player_fire_weapon(
-    mut commands: Commands,
-    keyboard_input: Res<Input<MouseButton>>,
-    player_query: Query<(&mut Player, &mut Transform)>
-)
-{
-    const BULLET_SPEED: f32 = 4.0;
-    let (player, transform) = player_query.single();
-
-    // why does this work? https://www.reddit.com/r/rust_gamedev/comments/rphgsf/calculating_bullet_x_and_y_position_based_off_of/
-    let velocity = ((transform.rotation * Vec3::Y) * BULLET_SPEED) + Vec3::new(player.delta_x, player.delta_y, 0.0);
-
-    // should be just pressed, but it's fun with keyboard_input.pressed()
-    if keyboard_input.just_pressed(MouseButton::Left) {
-        let player_shape = shapes::Circle {
-            ..shapes::Circle::default()
-        };
-    
-        commands.spawn()
-            .insert(Projectile {
-                velocity: Vec2 { x: velocity.x, y: velocity.y },
-                timer: Timer::from_seconds(5.0, false),
-                hitbox: HitboxCircle { radius: 2.0 }
-            })
-            .insert_bundle(GeometryBuilder::build_as(
-                &player_shape,
-                DrawMode::Outlined {
-                    fill_mode: FillMode::color(Color::DARK_GRAY),
-                    outline_mode: StrokeMode::new(Color::RED, 1.0),
-                },
-                transform.clone(),
-            ))
-            .insert(Collider);
     }
 }
 
---
+++ src/player.rs
@@ -0,0 +1,210 @@
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+use bevy_inspector_egui::{Inspectable, RegisterInspectable};
+use bevy::render::camera::RenderTarget;
+use crate::{ HitboxCircle, Crosshair, Projectile, Collider, PI, TWO_PI };
+
+pub struct PlayerPlugin;
+
+#[derive(Component, Inspectable)]
+pub struct Player {
+    pub delta_x: f32,
+    pub delta_y: f32,
+    pub delta_rotation: f32,
+    pub hitbox: HitboxCircle
+}
+
+impl Player {
+    fn new() -> Player {
+        Player { 
+            delta_x: 0.0,
+            delta_y: 0.0,
+            delta_rotation: 0.0,
+            hitbox: HitboxCircle { radius: 5.0 }
+        }
+    }
+}
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        // add things to your app here
+
+        app
+            .add_startup_system(Self::spawn_player)
+            .add_system(Self::player_movement)
+            .add_system(Self::player_fire_weapon)
+            .register_inspectable::<Player>();
+
+    }
+}
+
+impl PlayerPlugin {
+    fn spawn_player(
+        mut commands: Commands
+    ) {
+        let player_shape = shapes::RegularPolygon {
+            sides: 3,
+            feature: shapes::RegularPolygonFeature::Radius(20.0),
+            ..shapes::RegularPolygon::default()
+        };
+    
+        let mut player = commands.spawn()
+            .insert(Player::new())
+            .insert_bundle(GeometryBuilder::build_as(
+                &player_shape,
+                DrawMode::Outlined {
+                    fill_mode: FillMode::color(Color::CYAN),
+                    outline_mode: StrokeMode::new(Color::WHITE, 2.0),
+                },
+                Transform {
+                    scale: Vec3::new(0.5, 1.0, 1.0),
+                    ..Default::default()
+                }
+                // Transform::default(),
+            ));
+    }
+
+    fn player_movement(
+        keyboard_input: Res<Input<KeyCode>>,
+        // need to get window dimensions
+        wnds: Res<Windows>,
+        // query to get camera transform
+        q_camera: Query<(&Camera, &GlobalTransform)>,
+        mut player_query: Query<(&mut Player, &mut Transform), Without<Crosshair>>,
+        mut crosshair_query: Query<(&mut Crosshair, &mut Path, &mut Transform)>
+    ) {
+    
+        // get the camera info and transform
+        // assuming there is exactly one main camera entity, so query::single() is OK
+        let (camera, camera_transform) = q_camera.single();
+    
+        let (crosshair, mut path, mut crosshair_transform) = crosshair_query.single_mut();
+    
+        // get the window that the camera is displaying to (or the primary window)
+        let wnd = if let RenderTarget::Window(id) = camera.target {
+            wnds.get(id).unwrap()
+        } else {
+            wnds.get_primary().unwrap()
+        };
+    
+        const ACCELERATION: f32 = 0.2;
+        const DECLERATION: f32 = 0.95;
+        const SPIN_ACCELERATION: f32 = 0.4;
+        const SPIN_DECELERATION: f32 = 0.1;
+        const MAX_VELOCITY: f32 = 6.0;
+    
+        let (mut player, mut player_trans) = player_query.single_mut();
+    
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            player.delta_x -= ACCELERATION;
+        }
+    
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            player.delta_x += ACCELERATION;
+        }
+    
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            player.delta_y += ACCELERATION;
+        }
+    
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            player.delta_y -= ACCELERATION;
+        }
+    
+         // check if the cursor is inside the window and get its position
+         if let Some(screen_pos) = wnd.cursor_position() {
+            // get the size of the window
+            let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+    
+            // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
+            let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+    
+            // matrix for undoing the projection and camera transform
+            let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+    
+            // use it to convert ndc to world-space coordinates
+            let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+    
+            // reduce it to a 2D value
+            let world_pos: Vec2 = world_pos.truncate();
+    
+            let player_to_mouse = Vec2::new(player_trans.translation.x, player_trans.translation.y) - world_pos;
+            let ship_angle_difference = Vec2::angle_between(player_to_mouse, (player_trans.rotation * Vec3::Y).truncate());
+    
+            //Rotate towards position mouse is on
+            if ship_angle_difference > 0.0 {
+                player.delta_rotation += SPIN_ACCELERATION * (TWO_PI - ship_angle_difference.abs());
+            } else
+    
+            if ship_angle_difference < 0.0 {
+                player.delta_rotation -= SPIN_ACCELERATION * (TWO_PI - ship_angle_difference.abs());
+            }
+    
+            // Draw Crosshair
+            {
+                let line = shapes::Line (
+                    player_trans.translation.truncate(),
+                    world_pos,
+                );
+        
+                *path = ShapePath::build_as(&line);
+            }
+            
+            // eprintln!("World coords: {}/{}", world_pos.x, world_pos.y);
+        }
+    
+        player.delta_x = player.delta_x.clamp(-MAX_VELOCITY, MAX_VELOCITY);
+        player.delta_y = player.delta_y.clamp(-MAX_VELOCITY, MAX_VELOCITY);
+        player.delta_rotation = player.delta_rotation.clamp(-MAX_VELOCITY, MAX_VELOCITY);
+    
+        player_trans.translation.x += player.delta_x;
+        player_trans.translation.y += player.delta_y;
+    
+        // player_trans.translation.x = player_trans.translation.x.clamp(-320.0, 320.0);
+        // player_trans.translation.y = player_trans.translation.y.clamp(-320.0, 320.0);
+    
+        player_trans.rotate_z(player.delta_rotation.to_radians());
+    
+        // Decelerate
+        player.delta_x *= DECLERATION;
+        player.delta_y *= DECLERATION;
+        player.delta_rotation *= SPIN_DECELERATION;
+    
+    }
+
+    fn player_fire_weapon(
+        mut commands: Commands,
+        keyboard_input: Res<Input<MouseButton>>,
+        player_query: Query<(&mut Player, &mut Transform)>
+    )
+    {
+        const BULLET_SPEED: f32 = 4.0;
+        let (player, transform) = player_query.single();
+    
+        // why does this work? https://www.reddit.com/r/rust_gamedev/comments/rphgsf/calculating_bullet_x_and_y_position_based_off_of/
+        let velocity = ((transform.rotation * Vec3::Y) * BULLET_SPEED) + Vec3::new(player.delta_x, player.delta_y, 0.0);
+    
+        // should be just pressed, but it's fun with keyboard_input.pressed()
+        if keyboard_input.just_pressed(MouseButton::Left) {
+            let player_shape = shapes::Circle {
+                ..shapes::Circle::default()
+            };
+        
+            commands.spawn()
+                .insert(Projectile {
+                    velocity: Vec2 { x: velocity.x, y: velocity.y },
+                    timer: Timer::from_seconds(5.0, false),
+                    hitbox: HitboxCircle { radius: 2.0 }
+                })
+                .insert_bundle(GeometryBuilder::build_as(
+                    &player_shape,
+                    DrawMode::Outlined {
+                        fill_mode: FillMode::color(Color::DARK_GRAY),
+                        outline_mode: StrokeMode::new(Color::RED, 1.0),
+                    },
+                    transform.clone(),
+                ))
+                .insert(Collider);
+        }
+    }
+}
