77ac11efb943f6755c1580b598d55bb860ff4ce5
Andrew Exton <andrew@affinitymws.com>
2022-09-24 21:46:52 -0700

    astroids split on projectile contact.

--- src/projectile.rs
+++ src/projectile.rs
@@ -1,7 +1,8 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 use bevy_prototype_lyon::prelude as lyon;
-use std::f32::consts::PI;
 use rand::Rng;
 use crate::{astroid::{AstroidPlugin, Astroid, AstroidSize}, PIXELS_PER_METER};
 
@@ -17,7 +18,6 @@ pub struct ProjectilePlugin;
 impl Plugin for ProjectilePlugin {
     fn build(&self, app: &mut App) {
         app
-            // .add_system(Self::projectile_movement)
             .add_system(Self::handle_projectile_collision_event);
     }
 }
@@ -55,52 +55,72 @@ impl ProjectilePlugin {
             .insert(Collider::ball(projectile_shape.radius))
             .insert(Transform::from_translation(spawn_position.extend(0.0)))
             .insert(ActiveEvents::COLLISION_EVENTS)
-            .insert(ActiveHooks::FILTER_CONTACT_PAIRS)
+            // .insert(ActiveHooks::FILTER_CONTACT_PAIRS)
             .insert(Restitution::coefficient(0.01));
     }
 
-    // fn projectile_movement(
-    //     mut commands: Commands,
-    //     mut projectile_query: Query<(Entity, &mut Projectile, &mut Transform)>,
-    //     time: Res<Time>
-    // )
-    // {
-    //     for (ent, mut projectile, mut transform) in projectile_query.iter_mut() {
-    //         transform.translation.x += projectile.velocity.x;
-    //         transform.translation.y += projectile.velocity.y;
-    
-    //         projectile.timer.tick(time.delta());
-    
-    //         if projectile.timer.finished() {
-    //             commands.entity(ent).despawn_recursive();
-    //         }
-    //     }
-    // }
-
     fn handle_projectile_collision_event(
         mut contact_events: EventReader<CollisionEvent>,
-        astroid_query: Query<(Entity, &Astroid), With<Astroid>>,
-        projectile_query: Query<(Entity, &Projectile), With<Projectile>>,
+        astroid_query: Query<(Entity, &Astroid, &Transform, &Velocity), With<Astroid>>,
+        projectile_query: Query<(Entity, &Projectile, &Velocity), With<Projectile>>,
         time: Res<Time>,
         mut commands: Commands,
     ) {
         for contact_event in contact_events.iter() {
-            if let CollisionEvent::Started(h1, h2, _event_flag) = contact_event {
+            for (projectile_ent, projectile, projectile_velocity) in projectile_query.iter() {
+                for (astroid_ent, astroid, astroid_transform, astroid_velocity) in astroid_query.iter() {
+                
+                    if let CollisionEvent::Started(h1, h2, _event_flag) = contact_event {
 
-                //FIXME: there's got to be a better way...
-                // h1 is project, despawn it.
-                // if projectile_query.contains(*h1) && astroid_query.contains(*h2) {
-                //     println!("PROJECTILE COLLISION WITH ASTROID");
-                //     commands.entity(*h1).despawn_recursive();
-                // }
+                        if (projectile_ent == *h2 && astroid_ent == *h1) ||
+                            (projectile_ent == *h1 && astroid_ent == *h2) {
+    
+                            println!("PROJECTILE COLLISION WITH ASTROID");
 
-                // h2 is projectile, despawn it.
-                // why does this work, but the above implementation doesn't???
-                if projectile_query.contains(*h2) && astroid_query.contains(*h1) {
-                    println!("PROJECTILE COLLISION WITH ASTROID");
-                    commands.entity(*h2).despawn_recursive();
+                            let mut rng = rand::thread_rng();
+                            let split_angle = rng.gen_range(0.0..PI/4.0);
+                            
+                            let right_velocity = projectile_velocity.linvel.rotate(Vec2::from_angle(split_angle)) * 0.5;
+                            let left_velocity = projectile_velocity.linvel.rotate(Vec2::from_angle(-split_angle)) * 0.5;
+    
+                            match &astroid.size {
+                                AstroidSize::Small => {
+                                },
+                                AstroidSize::Medium => {
+                                    AstroidPlugin::spawn_astroid(&mut commands, AstroidSize::Small, right_velocity, astroid_transform.translation.truncate());
+                                    AstroidPlugin::spawn_astroid(&mut commands, AstroidSize::Small, left_velocity, astroid_transform.translation.truncate());
+    
+                                },
+                                AstroidSize::Large => {
+                                    AstroidPlugin::spawn_astroid(&mut commands, AstroidSize::Medium, right_velocity,astroid_transform.translation.truncate());
+                                    AstroidPlugin::spawn_astroid(&mut commands, AstroidSize::Medium, left_velocity, astroid_transform.translation.truncate());
+                                }
+                            }
+    
+                            commands.entity(projectile_ent).despawn_recursive();
+                            commands.entity(astroid_ent).despawn_recursive();
+    
+                        }
+    
+                        //FIXME: there's got to be a better way...
+                        // h1 is project, despawn it.
+                        // if projectile_query.contains(*h1) && astroid_query.contains(*h2) {
+                        //     println!("PROJECTILE COLLISION WITH ASTROID");
+                        //     commands.entity(*h1).despawn_recursive();
+                        // }
+        
+                        // h2 is projectile, despawn it.
+                        // why does this work, but the above implementation doesn't???
+                        // if projectile_query.contains(*h2) && astroid_query.contains(*h1) {
+                        //     println!("PROJECTILE COLLISION WITH ASTROID");
+                        //     commands.entity(*h2).despawn_recursive();
+                        // }
+                    }
+                
                 }
+                
             }
+            
         }
 
     }
