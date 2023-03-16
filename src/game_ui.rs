bc235fc28f9a1a4ca0816c1e6bb72a3b8c585f85
andrewexton373 <aexton@calpoly.edu>
2023-03-16 01:36:40 -0700

    cleanup ui.

--- src/game_ui.rs
+++ src/game_ui.rs
@@ -90,7 +90,7 @@ impl GameUIPlugin {
         mut contexts: EguiContexts,
         mut inventory_query: Query<(&Player, &mut Inventory)>,
     ) {
-        egui::Window::new("DND Ship Inventory").anchor(Align2::LEFT_BOTTOM, Vec2::ZERO).show(contexts.ctx_mut(), |ui| {
+        egui::Window::new("DND Ship Inventory").auto_sized().anchor(Align2::LEFT_BOTTOM, Vec2::ZERO).show(contexts.ctx_mut(), |ui| {
 
             let (_, mut inventory) = inventory_query.single_mut();
 
@@ -98,7 +98,9 @@ impl GameUIPlugin {
             ui.label(format!("Capacity: {:.2}%", inventory_capacity_percent));
             ui.label(Self::progress_string(inventory_capacity_percent / 100.0));
 
-            let response =
+            ui.group(|ui| {
+
+                let response =
                 // make sure this is called in a vertical layout.
                 // Horizontal sorting is not supported yet.
                 
@@ -119,6 +121,10 @@ impl GameUIPlugin {
             if let Some(response) = response.completed {
                 shift_vec(response.from, response.to, &mut inventory.items);
             }
+
+            });
+
+            
         });
     }
 
@@ -156,111 +162,139 @@ impl GameUIPlugin {
         let cc = cc_res.0.clone();
         egui::SidePanel::right("BaseStation Contextual Menu").show_animated(contexts.ctx_mut(), cc.contains(&ContextClue::NearBaseStation),|ui| {
 
-            ui.heading("Base Station Inventory:");
-            ui.vertical(|ui| {
-                for item in inventory.items.clone() {
-                    ui.label(format!("{:?}", item));
+            ui.group(|ui| {
+                ui.heading("Base Station Inventory:");
+                ui.vertical(|ui| {
+                    for item in inventory.items.clone() {
+                        ui.label(format!("{:?}", item));
+                    }
+                });
+            });
+            
+
+            ui.group(|ui| {
+                let refinery = refinery_query.single();
+
+                match &refinery.currently_processing {
+                    Some(recipe) => {
+
+                        ui.group(|ui| {
+                            ui.heading("Refinery Processing:");
+
+                            ui.label(format!("Currently Refining: {:?}", recipe));
+                            ui.label(format!("Time Remaining: {:.1} sec", refinery.remaining_processing_time));
+                            ui.label(Self::progress_string( (recipe.time_required - refinery.remaining_processing_time) / recipe.time_required));
+                        });
+
+                    },
+                    None => {}
+                }
+        
+                ui.heading("Refine Raw Ores:");
+
+                for recipe in &refinery.recipes {
+                    ui.group(|ui| {
+                        ui.vertical(|ui| {
+                            ui.horizontal(|ui| {
+                                ui.label(format!("{:?}", recipe.item_created));
+                                ui.label(format!("Requires: {:?}", recipe.items_required));
+                                if inventory.has_items(recipe.items_required.clone()) { ui.label("👍");}
+                            });
+    
+                            ui.horizontal(|ui| {
+                                ui.label(format!("Time Required: {:.1} sec", recipe.time_required));
+    
+                                if ui.button("Smelt").clicked() {
+                                    smelt_events.send(SmeltEvent(recipe.clone()));
+                                }
+                            })
+                        });
+                    });
+
                 }
             });
 
+            ui.group(|ui| {
 
-            let refinery = refinery_query.single();
+                let factory = factory_query.single();
 
-            match &refinery.currently_processing {
-                Some(recipe) => {
-                    ui.heading("Refinery Processing:");
+                match &factory.currently_processing {
+                    Some(recipe) => {
 
-                    ui.label(format!("Currently Refining: {:?}", recipe));
-                    ui.label(format!("Time Remaining: {:.1} sec", refinery.remaining_processing_time));
-                    ui.label(Self::progress_string( (recipe.time_required - refinery.remaining_processing_time) / recipe.time_required));
-                },
-                None => {}
-            }
-    
-            ui.heading("Refine Raw Ores:");
-
-            for recipe in &refinery.recipes {
-                ui.vertical(|ui| {
-                    ui.horizontal(|ui| {
-                        ui.label(format!("{:?}", recipe.item_created));
-                        ui.label(format!("Requires: {:?}", recipe.items_required));
-                        if inventory.has_items(recipe.items_required.clone()) { ui.label("👍");}
-                    });
-
-                    ui.horizontal(|ui| {
-                        ui.label(format!("Time Required: {:.1} sec", recipe.time_required));
-
-                        if ui.button("Smelt").clicked() {
-                            smelt_events.send(SmeltEvent(recipe.clone()));
-                        }
-                    })
-                });
-            }
-
-            let factory = factory_query.single();
-
-            match &factory.currently_processing {
-                Some(recipe) => {
-                    ui.heading("Factory Processing:");
-                    ui.label(format!("Currently Crafting: {:?}", recipe.item_created));
-                    ui.label(format!("Time Remaining: {:.1} sec", factory.remaining_processing_time));
-                    ui.label(Self::progress_string( (recipe.time_required - factory.remaining_processing_time) / recipe.time_required));
-                },
-                None => {}
-            }
-    
-            ui.heading("Factory Recipes:");
-            for recipe in &factory.recipes {
-
-                ui.vertical(|ui| {
-                    ui.horizontal(|ui| {
-                        ui.label(format!("{:?}", recipe.item_created));
-                        
-                        ui.label(format!("Requires: {:?}", recipe.items_required));
-                        if inventory.has_items(recipe.items_required.clone()) { ui.label("👍");}
-                        
-                    });
-
-                    ui.horizontal(|ui| {
-                        ui.label(format!("Time Required: {:.1} sec", recipe.time_required));
-
-                        if ui.button("Craft").clicked() {
-                            craft_events.send(CraftEvent(recipe.clone()));
-                        }
-                    })
-                });
-
-            }
-
-            let (_, upgrades) = player_query.single();
-
-            ui.heading("Ship Upgrades:");
-
-            for upgrade in &upgrades.upgrades {
-                    ui.horizontal(|ui| {
-
-                        ui.vertical(|ui| {
-
-                            ui.label(format!("{:?}", upgrade));
-                            if ui.button("Upgrade").clicked() {
-                                upgrade_events.send(UpgradeEvent(upgrade.clone()));
-                            }
+                        ui.group(|ui| {
+                            ui.heading("Factory Processing:");
+                            ui.label(format!("Currently Crafting: {:?}", recipe.item_created));
+                            ui.label(format!("Time Remaining: {:.1} sec", factory.remaining_processing_time));
+                            ui.label(Self::progress_string( (recipe.time_required - factory.remaining_processing_time) / recipe.time_required));
                         });
-                        
-                        ui.vertical(|ui| {
-                            ui.label("Requires: ");
-                            if upgrade.requirements().is_some() {
-                                for requirement in upgrade.requirements().unwrap().requirements { // TODO: This seems unnecessairly convoluted..
-                                    ui.label(format!("{:?}", requirement));
+
+                    },
+                    None => {}
+                }
         
+                ui.heading("Factory Recipes:");
+                for recipe in &factory.recipes {
+
+                    ui.group(|ui| {
+                        ui.vertical(|ui| {
+                            ui.horizontal(|ui| {
+                                ui.label(format!("{:?}", recipe.item_created));
+                                
+                                ui.label(format!("Requires: {:?}", recipe.items_required));
+                                if inventory.has_items(recipe.items_required.clone()) { ui.label("👍");}
+                                
+                            });
+    
+                            ui.horizontal(|ui| {
+                                ui.label(format!("Time Required: {:.1} sec", recipe.time_required));
+    
+                                if ui.button("Craft").clicked() {
+                                    craft_events.send(CraftEvent(recipe.clone()));
                                 }
-                            }
+                            })
                         });
-            
                     });
 
-            }
-        
+                }
+
+            });
+            
+            ui.group(|ui| {
+
+                let (_, upgrades) = player_query.single();
+
+                ui.heading("Ship Upgrades:");
+    
+                for upgrade in &upgrades.upgrades {
+
+                    ui.group(|ui| {
+                        ui.horizontal(|ui| {
+
+                            ui.vertical(|ui| {
+    
+                                ui.label(format!("{:?}", upgrade));
+                                if ui.button("Upgrade").clicked() {
+                                    upgrade_events.send(UpgradeEvent(upgrade.clone()));
+                                }
+                            });
+                            
+                            ui.vertical(|ui| {
+                                ui.label("Requires: ");
+                                if upgrade.requirements().is_some() {
+                                    for requirement in upgrade.requirements().unwrap().requirements { // TODO: This seems unnecessairly convoluted..
+                                        ui.label(format!("{:?}", requirement));
+            
+                                    }
+                                }
+                            });
+                
+                        });
+                    });
+
+                }
+
+            });
+
         });
 
     }
@@ -272,17 +306,30 @@ impl GameUIPlugin {
         let (player, velocity) = player_query.single();
     
         egui::Window::new("Ship Information").anchor(Align2::LEFT_TOP, Vec2::ZERO).show(contexts.ctx_mut(), |ui| {
-            ui.label(format!("Health: {:.2}%", player.health.current()));
-            let health_percent = player.health.current() / 100.0;
-            ui.label(Self::progress_string(health_percent));
+            ui.vertical_centered_justified(|ui| {
+                ui.horizontal(|ui| {
+                    ui.group(|ui| {
+                        ui.label(format!("Health: {:.2}%", player.health.current()));
+                        let health_percent = player.health.current() / 100.0;
+                        ui.label(Self::progress_string(health_percent));
+                    });
+        
+                    ui.group(|ui| {
+                        ui.label(format!("Battery: {:.2}KWh", player.battery.current()));
+                        let battery_percent = player.battery.current() / 1000.0;
+                        ui.label(Self::progress_string(battery_percent));
+                    });
+                    
+                });
     
-            ui.label(format!("Battery: {:.2}KWh", player.battery.current()));
-            let battery_percent = player.battery.current() / 1000.0;
-            ui.label(Self::progress_string(battery_percent));
-    
-            ui.label(format!("Speed: {:.2}", velocity.linvel.length()));
-            ui.add(egui::Slider::new(&mut player.engine.power_level.clone(), 0.0..=100.0).text("Engine Power"));
-            // ui.label(format!("Direction: {:.2}", player.1.linvel.angle_between(Vec2::X)));
+                ui.horizontal(|ui| {
+                    ui.add(egui::Slider::new(&mut player.engine.power_level.clone(), 0.0..=100.0).text("Engine Power"));
+                    ui.label(format!("Speed: {:.2}", velocity.linvel.length()));
+                    // ui.label(format!("Direction: {:.2}", player.1.linvel.angle_between(Vec2::X)));
+                }); 
+            });
+            
+            
         });
     }
     
