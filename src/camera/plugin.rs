e5eac580885d651168313c8edb674fc4b9d015fa
Andrew Exton <andrewexton373@gmail.com>
2024-02-26 01:46:45 -0800

    fixed player jitter see: https://github.com/Jondolf/bevy_xpbd/issues/211#issuecomment-1789342920

--- src/camera/plugin.rs
+++ src/camera/plugin.rs
@@ -1,4 +1,5 @@
-use bevy::app::{App, Plugin, Startup, Update};
+use bevy::{app::{App, Plugin, PostUpdate, Startup, Update}, ecs::schedule::IntoSystemConfigs, transform::TransformSystem};
+use bevy_xpbd_2d::PhysicsSet;
 
 use super::systems::{camera_follows_target, setup_camera};
 
@@ -8,8 +9,10 @@ impl Plugin for GameCameraPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Startup, setup_camera)
-            .add_systems(Update, (
+            .add_systems(PostUpdate, (
                 camera_follows_target
+                    .after(PhysicsSet::Sync)
+                    .before(TransformSystem::TransformPropagate)
             ));
     }
 }
