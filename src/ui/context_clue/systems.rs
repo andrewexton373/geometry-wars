0b50e7d8c5a3cbeaeff76864dbc95521878063fd
Andrew Exton <andrewexton373@gmail.com>
2026-01-18 14:43:40 -0800

    cleanup

--- src/ui/context_clue/systems.rs
+++ src/ui/context_clue/systems.rs
@@ -6,29 +6,21 @@ use bevy_egui::{
 
 use super::resources::ContextClues;
 
-pub fn ui_context_clue(mut contexts: EguiContexts, context_clues_res: Res<ContextClues>) {
+pub fn ui_context_clue(mut ctx: EguiContexts, context_clues_res: Res<ContextClues>) {
     let cc = &context_clues_res.0;
     if cc.is_empty() {
         return;
     }
 
-    let ctx = match contexts.ctx_mut() {
-        Ok(ctx) => ctx,
-        Err(_) => return, // or log + return
-    };
-
     egui::Window::new("Context Clue")
         .title_bar(false)
         .resizable(false)
         .anchor(egui::Align2::CENTER_BOTTOM, egui::vec2(0.0, -100.0))
-        .show(ctx, |ui| {
+        .show(ctx.ctx_mut().expect("No Context"), |ui| {
             ui.vertical(|ui| {
                 for clue in cc.iter() {
                     ui.label(clue.text());
                 }
             });
-
-            // If you need “available rect”, do it HERE:
-            // let rect = ui.available_rect_before_wrap();
         });
 }
